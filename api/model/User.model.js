4d78f33741c1f5df6382db4ba9a61f3b26040a76
Binary Digitz <borisndoh@gmail.com>
2025-02-10 22:46:09 +0100

    Create user nodel

---
+++ api/model/User.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { unique } from "underscore";
+
+const UserSchema = mongoose.Schema({
+    username:{
+        required: true,
+        type:String,
+        minLength:3,
+        maxLength:50,
+        unique:true
+    },
+    email:{
+        required: true,
+        type:String,
+        minLength:3,
+        maxLength:50
+    },
+    password:{
+        required: true,
+        type:String,
+        minLength:3,
+        maxLength:50
+    }
+}, {timestaps: true})
+
+export const User = mongoose.model('User', UserSchema)
