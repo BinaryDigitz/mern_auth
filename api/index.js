f667cb2bc995381176d6513ce608de5e1be3f965
Binary Digitz <borisndoh@gmail.com>
2025-02-10 22:37:44 +0100

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,16 +1,29 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
 app.use(express.json())
+dotenv.config()
 
 const port = process.env.port || 3000
+const db = {
+    username: 'borisndoh',
+    password:'Y79a6Qnx603Lhl4X'
+}
 
-
-
-app.listen(port, () =>{
-    console.log(`App listening on port ${port}`);
+    mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('Connected to database.....');
+    
+    app.listen(port, () =>{
+     console.log(`App listening on port ${port}`)
+        
+    })
+}).catch(err => {
+    console.log(err)
     
 })
+
 
 
 
